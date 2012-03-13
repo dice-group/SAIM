2e5c634900d1a78c6f0a5c7a0188bf99921ce5cb
Klaus Lyko <mail4klaus@gmx@gmx.de>
2012-03-13 12:22:55 +0100

    Adding grindpanel with 3 rows and one column for Menu/Caption, one for toolbar and the main panel for dialogs and other components

--- src/de/uni_leipzig/simba/saim/SAIMApplication.java
+++ src/de/uni_leipzig/simba/saim/SAIMApplication.java
@@ -10,26 +10,45 @@ import de.uni_leipzig.simba.saim.gui.widget.KBInfoDialog;
 public class SAIMApplication extends Application {
 	
 	private final Window mainWindow;
-	private Layout mainLayout;
+	private AbsoluteLayout mainLayout;
+	private GridLayout gridLayout;
+	
+	
 	
 	public SAIMApplication() {
 		mainWindow = new Window("saim");		
-		mainLayout = new HorizontalLayout();		
+		mainLayout = buildMainLayout();
 		mainWindow.setContent(mainLayout);
+	}
+	
+	private AbsoluteLayout buildMainLayout() {
+		// common part: create layout
+		mainLayout = new AbsoluteLayout();
+		mainLayout.setWidth("100%");
+		mainLayout.setHeight("100%");
+		mainLayout.setMargin(false);
 		
-		//mainWindow.addComponent(form);
+		// gridLayout
+		gridLayout = new GridLayout();
+		gridLayout.setImmediate(false);
+		gridLayout.setWidth("320px");
+		gridLayout.setHeight("240px");
+		gridLayout.setMargin(false);		
+		gridLayout.setRows(3);
+		
+		mainLayout.addComponent(gridLayout);
+		// filling Layouts
+		Label label = new Label("SAIM user interface");
+		Label label2 = new Label("GridPos(0, 2)");
+		gridLayout.addComponent(label, 0, 0);
+		gridLayout.addComponent(label2, 0, 2);
+	
+		return mainLayout;
 	}
 	
 	@Override
-	public void init() {
-
-		
-		Label label = new Label("SAIM user interface");
-		
-		addButtons();
-		
-		mainLayout.addComponent(label);
-				
+	public void init() {		
+		addButtons();				
 		setMainWindow(mainWindow);		
 	}
 	
@@ -40,10 +59,10 @@ public class SAIMApplication extends Application {
 			public void buttonClick(ClickEvent event) {
 				KBInfoDialog kbI = new KBInfoDialog(mainWindow, "Configure Source endpoint");
 				mainWindow.addWindow(kbI);
-			}
+				}
 		});
 		
-		Button openKBTargetDialoge = new Button("Configure source");
+		Button openKBTargetDialoge = new Button("Configure target");
 		openKBTargetDialoge.addListener(new ClickListener() {			
 			@Override
 			public void buttonClick(ClickEvent event) {
@@ -51,8 +70,10 @@ public class SAIMApplication extends Application {
 				mainWindow.addWindow(kbI);
 			}
 		});
-		mainLayout.addComponent(openKBSourceDialoge);
-		mainLayout.addComponent(openKBTargetDialoge);
+		HorizontalLayout hor = new HorizontalLayout();
+		hor.addComponent(openKBSourceDialoge);
+		hor.addComponent(openKBTargetDialoge);
+		gridLayout.addComponent(hor, 0, 1);
 	}
 
 }
