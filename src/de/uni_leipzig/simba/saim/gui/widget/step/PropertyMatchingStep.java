9c60740ba74d6519238ad1278086eb743471ab16
Konrad Höffner <konradh.hoeffner@uni-leipzig.de>
2012-03-15 12:06:40 +0100

    forgot to add some files

---
+++ src/de/uni_leipzig/simba/saim/gui/widget/step/PropertyMatchingStep.java
@@ -0,0 +1,33 @@
+package de.uni_leipzig.simba.saim.gui.widget.step;
+
+import org.vaadin.teemu.wizards.WizardStep;
+
+import com.vaadin.ui.Component;
+
+import de.uni_leipzig.simba.saim.gui.widget.EndpointPanel;
+
+public class PropertyMatchingStep implements WizardStep
+{
+
+	@Override
+	public String getCaption() {return "Property Matching";}
+
+	@Override
+	public Component getContent()
+	{
+		return new EndpointPanel();
+	}
+
+	@Override
+	public boolean onAdvance()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean onBack()
+	{
+		return true;
+	}
+
+}
