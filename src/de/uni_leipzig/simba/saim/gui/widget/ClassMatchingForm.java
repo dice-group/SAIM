543aed3d20bfe92b54c1e0d5b6f648ba11c90e24
Klaus Lyko <mail4klaus@gmx@gmx.de>
2012-03-20 11:31:58 +0100

    adding restrictions according to the class matching panel

--- src/de/uni_leipzig/simba/saim/gui/widget/ClassMatchingForm.java
+++ src/de/uni_leipzig/simba/saim/gui/widget/ClassMatchingForm.java
@@ -1,25 +1,13 @@
 package de.uni_leipzig.simba.saim.gui.widget;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import com.vaadin.data.Item;
-import com.vaadin.data.Property;
-import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.event.ItemClickEvent;
 import com.vaadin.event.ItemClickEvent.ItemClickListener;
 import com.vaadin.ui.ComboBox;
-import com.vaadin.ui.DragAndDropWrapper;
 import com.vaadin.ui.Form;
-import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Layout;
-import com.vaadin.ui.Panel;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 
 import de.uni_leipzig.simba.io.KBInfo;
-import de.uni_leipzig.simba.saim.core.Pair;
 import de.uni_leipzig.simba.saim.gui.widget.ClassChooser.ClassNode;
 
 /** The class matching consists of two sets of classes from both knowledge bases and a match between them.
@@ -30,8 +18,10 @@ public class ClassMatchingForm extends Form
 	protected final ComboBox field; 
 //	protected final TextField target; 
 	protected final ClassChooser chooser;
+	KBInfo info;
 	
-	public ClassMatchingForm(String caption, KBInfo info) {
+	public ClassMatchingForm(String caption, final KBInfo info) {
+		this.info = info;
 		Layout layout = new VerticalLayout();
 		layout.setWidth("100%");
 		setLayout(layout);
@@ -45,7 +35,10 @@ public class ClassMatchingForm extends Form
 				field.setValue(event.getItemId());
 				ClassNode node = (ClassNode) event.getItemId();
 				field.addItem(node.url);
-				field.setValue(node.url);//				
+				field.setValue(node.url);
+				String rest = info.var +" rdf:type "+node.url;
+				info.restrictions.clear();
+				info.restrictions.add(rest);				
 			}
 		});
 		addField("textfield", field);
