6b3f089d70b7768ec269bc855a4a7ab411e5762b
Konrad Höffner <konradh.hoeffner@uni-leipzig.de>
2012-03-22 12:48:32 +0100

    improved layout for class matching suggestions.

--- src/de/uni_leipzig/simba/saim/gui/widget/ClassMatchingPanel.java
+++ src/de/uni_leipzig/simba/saim/gui/widget/ClassMatchingPanel.java
@@ -5,9 +5,10 @@ import org.vaadin.jonatan.contexthelp.ContextHelp;
 import com.github.wolfie.refresher.Refresher;
 import com.github.wolfie.refresher.Refresher.RefreshListener;
 import com.vaadin.ui.ComboBox;
-import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
+import com.vaadin.ui.ProgressIndicator;
 import com.vaadin.ui.VerticalLayout;
 
 import de.uni_leipzig.simba.saim.core.Configuration;
@@ -16,30 +17,39 @@ import de.uni_leipzig.simba.saim.core.Configuration;
 public class ClassMatchingPanel extends Panel
 {	
 	Configuration config = Configuration.getInstance();
-	final ComboBox suggestions = new ComboBox("Suggestions");
+	final ComboBox suggestionComboBox = new ComboBox();
 
 	protected void setupContextHelp()
 	{
 		ContextHelp contextHelp = new ContextHelp();
 		getContent().addComponent(contextHelp);
-		contextHelp.addHelpForComponent(suggestions, "Class pairs from LIMES.");
+		contextHelp.addHelpForComponent(suggestionComboBox, "Class pairs from LIMES.");
 	}
 
 	public ClassMatchingPanel()
 	{
 		setContent(new VerticalLayout());
-		{
-			FormLayout layout = new FormLayout(); // have the label to the right of the combobox and not on top
-			this.getContent().addComponent(layout);
-			suggestions.setWidth("100%");
-			suggestions.addItem("loading suggestions...");
-			suggestions.setValue("loading suggestions...");
-			layout.addComponent(suggestions);
-		}
+
+		//FormLayout layout = new FormLayout(); // have the label to the right of the combobox and not on top
+		HorizontalLayout layout = new HorizontalLayout();
+		layout.setSpacing(false);
+		layout.setWidth("100%");		
+		final ProgressIndicator progress = new ProgressIndicator();
+		progress.setSizeUndefined();
+		Label suggestionLabel = new Label("Suggestions");		
+		suggestionLabel.setSizeUndefined();		
+		layout.addComponent(suggestionLabel);
+		layout.addComponent(progress);
+		progress.setIndeterminate(true);				
+		suggestionComboBox.setWidth("100%");		
+		layout.addComponent(suggestionComboBox);
+		layout.setExpandRatio(suggestionComboBox, 1f);
+		suggestionComboBox.setVisible(false);
+		this.addComponent(layout);
 		{
 			HorizontalLayout hori = new HorizontalLayout();
 			hori.setWidth("100%");
-			suggestions.setEnabled(false);
+			suggestionComboBox.setEnabled(false);
 			new Thread()
 			{
 
@@ -51,8 +61,11 @@ public class ClassMatchingPanel extends Panel
 					refresher.addListener(listener);
 					addComponent(refresher);
 					try{Thread.sleep(5000);} catch (InterruptedException e) {	e.printStackTrace();}
-					suggestions.setEnabled(true);
-					suggestions.removeAllItems();
+					progress.setEnabled(false);
+					removeComponent(progress);
+					suggestionComboBox.setVisible(true);
+					suggestionComboBox.setEnabled(true);
+					suggestionComboBox.removeAllItems();
 					System.out.println("suggested enabled");					
 					listener.running=false;					
 				}
@@ -63,12 +76,11 @@ public class ClassMatchingPanel extends Panel
 		}
 		setupContextHelp();
 	}
-	
+
 	public class SuggestionsRefreshListener implements RefreshListener
 	{
 		boolean running = true; 
 		private static final long serialVersionUID = -8765221895426102605L;		    
 		@Override public void refresh(final Refresher source)	{if(!running) {removeComponent(source);source.setEnabled(false);}}
-	}
-	
+	}	
 }
