299c470d55ff053cf1dcd6147705b049cc3c0925
Klaus Lyko <mail4klaus@gmx@gmx.de>
2012-03-23 11:25:18 +0100

    Pair as a bean

--- src/de/uni_leipzig/simba/saim/core/Pair.java
+++ src/de/uni_leipzig/simba/saim/core/Pair.java
@@ -1,7 +1,24 @@
 package de.uni_leipzig.simba.saim.core;
 
-public class Pair<T>{
-	public final T a,b;
+public class Pair<T> {
+	private T a;
+	private T b;
+
+	public T getA() {
+		return a;
+	}
+
+	public T getB() {
+		return b;
+	}
+	
+	public void setA(T a) {
+		this.a = a;
+	}
+	
+	public void setB(T b) {
+		this.b = b;
+	}
 
 	public Pair(T a, T b) {
 		super();
@@ -12,4 +29,21 @@ public class Pair<T>{
 	public String toString() {
 		return a.toString()+" - "+b.toString();
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		if(this == o)
+			return true;
+		if(o == null)
+			return false;
+		if(o.getClass().equals(this.getClass())) {
+			Pair<String> oP = (Pair<String>) o;
+			if(oP.getA().equals(a) && oP.getB().equals(b))
+				return true;
+			return false;
+		}
+		return false;
+	}
+	
+	
 }
