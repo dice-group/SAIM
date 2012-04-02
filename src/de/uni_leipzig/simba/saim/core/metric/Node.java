95fabcc2080f521b3fd0336fea069bcd8564671a
Konrad Höffner <konradh.hoeffner@uni-leipzig.de>
2012-04-02 15:34:58 +0200

    more methods in node class.

--- src/de/uni_leipzig/simba/saim/core/metric/Node.java
+++ src/de/uni_leipzig/simba/saim/core/metric/Node.java
@@ -1,5 +1,6 @@
 package de.uni_leipzig.simba.saim.core.metric;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,7 +9,11 @@ public abstract class Node
 {
 	public final byte maxChilds = 0;
 	public abstract boolean validParentOf(Node n);
-	public Set<Node> childs = new HashSet<>();
+	protected Set<Node> childs = new HashSet<>();
+	
+	public Set<Node> getChilds() {return Collections.unmodifiableSet(childs);}
+	
+	public void addChild(Node n) {if(accepts(n)) childs.add(n);}
 	
 	public boolean accepts(Node n) {return validParentOf(n)&&childs.size()<maxChilds;}
 }
