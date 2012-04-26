70acc39a9fcac4f7a0185c4bc95267f9dfe6e394
Klaus Lyko <lyko.klaus@gmail.com>
2012-04-26 16:56:29 +0200

    externalizing self config strings new fixed limes version: Genetic Active Learner only producing valid metrics.

--- src/de/uni_leipzig/simba/saim/gui/widget/panel/SelfConfigPanel.java
+++ src/de/uni_leipzig/simba/saim/gui/widget/panel/SelfConfigPanel.java
@@ -17,20 +17,27 @@ import com.vaadin.ui.Panel;
 
 import de.uni_leipzig.simba.cache.HybridCache;
 import de.uni_leipzig.simba.io.KBInfo;
+import de.uni_leipzig.simba.saim.Messages;
 import de.uni_leipzig.simba.saim.core.Configuration;
+import de.uni_leipzig.simba.selfconfig.ComplexClassifier;
 import de.uni_leipzig.simba.selfconfig.MeshBasedSelfConfigurator;
 import de.uni_leipzig.simba.selfconfig.SimpleClassifier;
-
+/**
+ * Displays self configuration panel.
+ * @author Lyko
+ *
+ */
 public class SelfConfigPanel extends Panel{
 	
 	private Component parentComponent;
 	private Layout mainLayout;
 	MeshBasedSelfConfigurator bsc;
 	List<SimpleClassifier> classifiers;
+	ComplexClassifier cc;
 	final ProgressIndicator indicator = new ProgressIndicator();
 	final Panel stepPanel = new Panel();
 	Panel resultPanel;
-	Button nextRound;
+//	Button nextRound;
 	Button generateMetrik;
 	Select resultSelect = new Select();
 
@@ -48,10 +55,10 @@ public class SelfConfigPanel extends Panel{
 	 * Initialize all Panel.
 	 */
 	private void init() {
-		this.setCaption("Self Configuration interface");
+		this.setCaption(Messages.getString("SelfConfigPanel.caption")); //$NON-NLS-1$
 		mainLayout = new VerticalLayout();
 		this.setContent(mainLayout);
-		Label descriptor = new Label("Runs and controls the self configuration approach.");
+		Label descriptor = new Label(Messages.getString("SelfConfigPanel.description")); //$NON-NLS-1$
 		mainLayout.addComponent(descriptor);
 		Refresher refresher = new Refresher();
 		SelfConfigRefreshListener listener = new SelfConfigRefreshListener();
@@ -59,20 +66,19 @@ public class SelfConfigPanel extends Panel{
 		addComponent(refresher);
 
 		
-		indicator.setCaption("Progress");
+		indicator.setCaption(Messages.getString("SelfConfigPanel.progress")); //$NON-NLS-1$
 		mainLayout.addComponent(indicator);
 		indicator.setImmediate(true);
 
 		
-		stepPanel.setCaption("Starting self configuration");
+		stepPanel.setCaption(Messages.getString("SelfConfigPanel.panelcaption")); //$NON-NLS-1$
 		mainLayout.addComponent(stepPanel);
 
-		resultSelect.setCaption("Computed classifiers");
+		resultSelect.setCaption(Messages.getString("SelfConfigPanel.classifierlistcaption")); //$NON-NLS-1$
 		resultSelect.setNullSelectionAllowed(false);
 		
 
-		mainLayout.removeComponent(indicator);
-		mainLayout.removeComponent(stepPanel);
+		
 		resultPanel = new Panel();
 		mainLayout.addComponent(resultPanel);
 		// Buttons
@@ -81,11 +87,11 @@ public class SelfConfigPanel extends Panel{
 		resultLayout.addComponent(resultSelect);
 		resultLayout.addComponent(buttonLayout);
 		resultPanel.setContent(resultLayout);		
-		nextRound = new Button("Compute next round");
-		nextRound.addListener(new NextRoundButtonClickListener());
-		generateMetrik = new Button("Generate Metric");
+//		nextRound = new Button(Messages.getString("SelfConfigPanel.nextroundbutton")); //$NON-NLS-1$
+//		nextRound.addListener(new NextRoundButtonClickListener());
+		generateMetrik = new Button(Messages.getString("SelfConfigPanel.generatemetricbutton")); //$NON-NLS-1$
 		generateMetrik.addListener(new GenerateMetricButtonClickListener(mainLayout));
-		buttonLayout.addComponent(nextRound);
+//		buttonLayout.addComponent(nextRound);
 		buttonLayout.addComponent(generateMetrik);
 		
 		performSelfConfiguration();
@@ -95,26 +101,39 @@ public class SelfConfigPanel extends Panel{
 	 * PerformsSelfConfiguration
 	 */
 	protected void performSelfConfiguration() {
+		mainLayout.addComponent(indicator);
+		mainLayout.addComponent(stepPanel);
 		new Thread() {
 			public void run() {
 
 				float steps = 5f;
 				indicator.setValue(new Float(1f/steps));
 				indicator.requestRepaint();
-				stepPanel.setCaption("Getting source cache...");
+				stepPanel.setCaption(Messages.getString("SelfConfigPanel.sourcecache")); //$NON-NLS-1$
 				HybridCache sourceCache = HybridCache.getData(Configuration.getInstance().getSource());
 				indicator.setValue(new Float(2f/steps));
 				indicator.requestRepaint();
-				stepPanel.setCaption("Getting target cache...");
+				stepPanel.setCaption(Messages.getString("SelfConfigPanel.targetcache")); //$NON-NLS-1$
 				HybridCache targetCache = HybridCache.getData(Configuration.getInstance().getTarget());
 				indicator.setValue(new Float(3f/steps));
-				stepPanel.setCaption("Performing self configuration...");
+				stepPanel.setCaption(Messages.getString("SelfConfigPanel.performselfconfig")); //$NON-NLS-1$
 				
 				bsc = new MeshBasedSelfConfigurator(sourceCache, targetCache, 0.6, 0.5);
-				classifiers = bsc.getBestInitialClassifiers();						
+				classifiers = bsc.getBestInitialClassifiers();
+				 
 				indicator.setValue(new Float(4f/steps));
+				stepPanel.setCaption(Messages.getString("SelfConfigPanel.gotinitialclassifiers")); //$NON-NLS-1$
 				
-				stepPanel.setCaption("Performed self configuration:");
+//				classifiers = bsc.learnClassifer(classifiers);
+				cc = bsc.getZoomedHillTop(5, 5, classifiers);
+				System.out.println(cc);
+				for(SimpleClassifier co:cc.classifiers) {
+					System.out.println(co);
+				}
+				System.out.println(cc.fMeasure);
+//				classifiers = cc.classifiers;
+				indicator.setValue(new Float(5f/steps));
+				stepPanel.setCaption(Messages.getString("SelfConfigPanel.complexclassifiercaption")+cc); //$NON-NLS-1$
 				showResults();
 
 			}
@@ -162,28 +181,39 @@ public class SelfConfigPanel extends Panel{
 		public void buttonClick(ClickEvent event) {
 			SimpleClassifier cl = (SimpleClassifier) resultSelect.getValue();
 			String metric = generateMetric(cl);
-			System.out.println(metric + " >= "+cl.threshold);
+			System.out.println(metric + " >= "+cl.threshold); //$NON-NLS-1$
 			Configuration.getInstance().setMetricExpression(metric);
 			Configuration.getInstance().setAcceptanceThreshold(cl.threshold);
 			l.removeAllComponents();
 			l.addComponent(new ExecutionPanel());
 		}		
-		private String generateMetric(SimpleClassifier cl) {
+		private String generateMetric(SimpleClassifier sl) {
 			KBInfo source=Configuration.getInstance().getSource();
 			KBInfo target=Configuration.getInstance().getTarget();
-			String metric = "";
+			String metric = ""; //$NON-NLS-1$
 			
-			metric += cl.measure+"("+source.var.replaceAll("\\?", "")+"."+cl.sourceProperty;
-			metric +=","+target.var.replaceAll("\\?", "")+"."+cl.targetProperty+")";
+			metric += sl.measure+"("+source.var.replaceAll("\\?", "")+"."+sl.sourceProperty; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+			metric +=","+target.var.replaceAll("\\?", "")+"."+sl.targetProperty+")|"+sl.threshold; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
 			return metric;
-		}		
+		}
+
+//		private String generateMetric(ComplexClassifier cl) {
+//			if(cl.classifiers.size()>1) {//more than a measure
+//				String complexMetric = "AND(";
+//				complexMetric+=generateMetric(cl.classifiers.get(0))+","+generateMetric(cl.classifiers.get(1));
+//				complexMetric+=")";
+//				return complexM
+//			} else {//multiple measures
+//				return generateMetric(cl.classifiers.get(0));
+//			}
+//		}
 	}
-	/**Controls Action taken by nextRound Button.*/
-	class NextRoundButtonClickListener implements Button.ClickListener {
-		@Override
-		public void buttonClick(ClickEvent event) {
-			classifiers = bsc.learnClassifer(classifiers);
-			showResults();
-		}		
-	}
+//	/**Controls Action taken by nextRound Button.*/
+//	class NextRoundButtonClickListener implements Button.ClickListener {
+//		@Override
+//		public void buttonClick(ClickEvent event) {
+//			classifiers = bsc.learnClassifer(classifiers);
+//			showResults();
+//		}		
+//	}
 }
