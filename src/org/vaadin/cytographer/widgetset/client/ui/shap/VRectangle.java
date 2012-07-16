a45c52ad5d9ead4b9b67cced9a66a857066782e7
Rene_Speck <rene.speck@web.de>
2012-07-16 13:03:25 +0200

    smaller size

--- src/org/vaadin/cytographer/widgetset/client/ui/shap/VRectangle.java
+++ src/org/vaadin/cytographer/widgetset/client/ui/shap/VRectangle.java
@@ -11,12 +11,15 @@ public class VRectangle extends Path {
 
 	public VRectangle(int x, int y, int size,String color) {
 		super(x,y);
+		
+		size=(int)(size*0.75); // smaller	
+		
 		moveRelativelyTo(-size,size);
 		lineRelativelyTo(size*2,0);
 		lineRelativelyTo(0, -size*2);
 		lineRelativelyTo(-size*2, 0);
 		close();
-		this.setFillColor(color); // blue
+		this.setFillColor(color); 
 	}
 
 	@Override
